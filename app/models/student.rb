34aaa6b04829a52144a078ff6f3d0b1c0009fc30
periwinklepath <hollyrgibson@gmail.com>
2013-07-13 16:55:29 -0500

    Change model student to inherit from user.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,4 @@
-class Student < ActiveRecord::Base
+class Student < User < ActiveRecord::Base
   belongs_to :user
   attr_accessible :address_1, :address_2, :blog, :city, :cohort, :country, :entrepreneur, :first_name, :github, :job_status, :last_name, :linkedin_id, :mentor, :personal_website, :phone_number, :postal_code, :state_province, :twitter_handle
 end
