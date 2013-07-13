b7f1cdfd91aae7b665027f621bd4015c10d001f5
periwinklepath <wha7ev3r@gmail.com>
2013-07-13 14:39:08 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mksdir::Application.config.secret_token = '5c73f1eab2bcd7c7b05be9e25d5089ac41a4782556f07d91c35b3e8d213d0c79e3bb7ebcc586e52b67f7dc8a960e2653f175f3dd872a4b4a00ae5993ee622d45'
