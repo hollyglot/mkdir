62684c26adbe109c9449de9a435cd5cdbb6c3284
periwinklepath <hollyrgibson@gmail.com>
2013-07-13 16:48:17 -0500

    Add model admin.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130713211918) do
+ActiveRecord::Schema.define(:version => 20130713213627) do
 
   create_table "students", :force => true do |t|
     t.string   "first_name"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(:version => 20130713211918) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "type"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
