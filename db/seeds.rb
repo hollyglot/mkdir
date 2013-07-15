95f2e9aef49f33255f1884da1049605f9ac7423a
Nick Gibson <aki.nicholas.gibson@gmail.com>
2013-07-15 10:14:06 -0500

    minor edits to seed info for an authenticated user

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@
 # See http://railsapps.github.io/rails-environment-variables.html
 
 
-puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
-puts 'user: ' << user.name
 
+
+puts 'DEFAULT USERS'
+user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup, :role => 'admin'
+puts 'user: ' << user.name
