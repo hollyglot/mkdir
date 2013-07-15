f99ed25b775f4f6951fa220514d60a6b2896e2fb
periwinklepath <wha7ev3r@gmail.com>
2013-07-15 12:01:25 -0500

    Update route for students create for rails_admin.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -47,7 +47,7 @@ class StudentsController < ApplicationController
 
     respond_to do |format|
       if @student.save
-        format.html { redirect_to @student, notice: 'Student was successfully created.' }
+        format.html { redirect_to students_path, notice: 'Student was successfully created.' }
         format.json { render json: @student, status: :created, location: @student }
       else
         format.html { render action: "new" }
