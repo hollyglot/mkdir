0c626ac6e1407ad244b45523ff1d4c6d9e4a73ca
MakerSquare Student <students@themakersquare.com>
2013-07-29 16:29:47 -0500

    removed files

--- db/migrate/20130726220117_add_gmaps_to_students.rb
+++
@@ -1,5 +0,0 @@
-class AddGmapsToStudents < ActiveRecord::Migration
-  add_column :students, :latitude,  :float #you can change the name, see wiki
-  add_column :students, :longitude, :float #you can change the name, see wiki
-  add_column :students, :gmaps, :boolean #not mandatory, see wiki
-end
