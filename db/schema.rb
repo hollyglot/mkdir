8074585fc3b943c73694f16bcceb1bb6ca7e74ed
Nick Gibson <aki.nicholas.gibson@gmail.com>
2013-07-30 15:03:46 -0500

    updated profile view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130726220117) do
+ActiveRecord::Schema.define(:version => 20130714224944) do
 
   create_table "rails_admin_histories", :force => true do |t|
     t.text     "message"
@@ -48,9 +48,6 @@ ActiveRecord::Schema.define(:version => 20130726220117) do
     t.string   "entrepreneur"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
-    t.float    "latitude"
-    t.float    "longitude"
-    t.boolean  "gmaps"
   end
 
   add_index "students", ["user_id"], :name => "index_students_on_user_id"
