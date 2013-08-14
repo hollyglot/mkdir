cf451466220f8c9b1df0a130df6dbcb91126f3a3
Nick Gibson <aki.nicholas.gibson@gmail.com>
2013-08-14 16:51:58 -0500

    removing asset files for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Mkdir::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( *.js *.scss *.coffee *.css )
-  config.assets.precompile += %w(rails_admin/rails_admin.css rails_admin/rails_admin.js)
+  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
--- public/assets/students-0229c36e8c983601cafa9a70cba10255.js
+++
@@ -1,1 +0,0 @@
-!function(){var a;a=function(){var a,n,e;return a=new google.maps.LatLng(30.265591,-97.742035),n=google.maps.MapTypeId.ROADMAP,e={center:a,zoom:12,mapTypeId:n},new google.maps.Map(this.map_canvas,e)},$(function(){var n;return n=a()})}.call(this);
