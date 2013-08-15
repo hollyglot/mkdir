3e7ac76b6596dce28e74b36cc43afa4b98c1ebac
Nick Gibson <aki.nicholas.gibson@gmail.com>
2013-08-14 19:42:21 -0500

    Testing asset compilation

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
   gem 'uglifier', '>= 1.0.3'
-  gem 'turbo-sprockets-rails3'
 end
 gem 'jquery-rails'
 gem 'bootstrap-sass'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,8 @@ Mkdir::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( *.js *.scss *.coffee *.css )
-  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
+  config.assets.precompile += %w( *.js *.scss *.coffee *.css )
+  # config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
