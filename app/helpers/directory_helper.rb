0db3d48f9f7e3bd3c605cd32ef21bd6367fe4f61
periwinklepath <wha7ev3r@gmail.com>
2013-08-15 16:39:52 -0500

    updated info window

--- app/helpers/directory_helper.rb
+++ app/helpers/directory_helper.rb
@@ -5,7 +5,15 @@ module DirectoryHelper
 
   def style_map_markers(people)
     people.to_gmaps4rails do |person, marker|
-      marker.infowindow render(:partial => "/directory/map-infowindow", :locals => { :person => person })
+      marker.infowindow "
+        <a href=\"/students/#{person.id}\">
+          <img src=\"#{person.pic}\" height=\"60\" width=\"60\" align=\"left\" style=\"margin-right:15px\">
+          <h4>
+            #{person.name}
+          </h4>
+        </a>
+        <p style=\"margin-top:-5px\"> #{person.developer_type}
+        </p>"
       marker.picture({
         :picture => image_path("#{person.class}.png"),
         :width   => 30,
