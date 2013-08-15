5587aa1a7b32b364cbe206d02529d9563545917a
Nick Gibson <aki.nicholas.gibson@gmail.com>
2013-08-15 16:16:15 -0500

    updated directory helper

--- app/helpers/directory_helper.rb
+++ app/helpers/directory_helper.rb
@@ -5,16 +5,17 @@ module DirectoryHelper
 
   def style_map_markers(people)
     people.to_gmaps4rails do |person, marker|
-        marker.infowindow "
-        <a href=\"/students/#{person.id}\">
-          <img src=\"#{person.pic}\" height=\"60\" width=\"60\" align=\"left\" style=\"margin-right:15px\">
-          <h4>
-            #{person.name}
-          </h4>
-        </a>
-        <p style=\"margin-top:-5px\"> #{person.developer_type}
-        </p>"
-      end
+      marker.infowindow render(:partial => "/directory/map-infowindow", :locals => { :person => person })
+      marker.picture({
+        :picture => image_path("#{person.class}.png"),
+        :width   => 30,
+        :height  => 30,
+        :marker_anchor => [5, 10],
+        :shadow_width => 110,
+        :shadow_height => 110,
+        :shadow_anchor => [5, 10],
+      })
+    end
   end
-  
+
 end
